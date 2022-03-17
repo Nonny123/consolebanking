a198d3718c83eddf7fb30a758195633a6980c8c3
Nonso <justcoolk@yahoo.com>
2022-03-17 16:37:44 +0100

    Create custom ApplicationException

--- InstantBanking/InstantBank.Entities/Customer.cs
+++ InstantBanking/InstantBank.Entities/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using InstantBank.Entities.Contracts;
+using InstantBank.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,12 @@ using System.Threading.Tasks;
 
 namespace InstantBank.Entities
 {
+    /// <summary>
+    /// Represents customer of the bank
+    /// </summary>
     public class Customer : ICustomer
     {
-        //fields
+        #region Private fields
         private Guid _customerID;
         private long _customerCode;
         private string _customerName;
@@ -18,15 +22,93 @@ namespace InstantBank.Entities
         private string _city;
         private string _country;
         private string _mobile;
+        #endregion
 
-        //properties
+        #region Public Properties
+        /// <summary>
+        /// Guid of Customer for Unique identfication
+        /// </summary>
         public Guid CustomerID { get => _customerID; set => _customerID = value; }
-        public long CustomerCode { get => _customerCode; set => _customerCode = value; }
-        public string CustomerName { get => _customerName; set => _customerName = value; }
+
+        /// <summary>
+        /// Auto-generated code number of the customer
+        /// </summary>
+        public long CustomerCode
+        {
+            get => _customerCode;
+            set
+            {
+                //customer code should be positive
+                if (value > 0)
+                {
+                    _customerCode = value;
+                }
+                else
+                {
+                    throw new CustomerException("Customer code should be positive only.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Name of the customer
+        /// </summary>
+        public string CustomerName
+        {
+            get => _customerName;
+            set
+            {
+                //customer name should be less than 40 characters
+                if (value.Length <= 40 && string.IsNullOrEmpty(value) == false)
+                {
+                    _customerName = value;
+                }
+                else
+                {
+                    throw new CustomerException("Customer Name should not be null and less than 40 characters long.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Address of the customer
+        /// </summary>
         public string Address { get => _address; set => _address = value; }
+
+        /// <summary>
+        /// Landmark of the customer's address
+        /// </summary>
         public string Landmark { get => _landmark; set => _landmark = value; }
+
+        /// <summary>
+        /// City of the customer
+        /// </summary>
         public string City { get => _city; set => _city = value; }
+
+        /// <summary>
+        /// Country of the customer
+        /// </summary>
         public string Country { get => _country; set => _country = value; }
-        public string Mobile { get => _mobile; set => _mobile = value; }
+
+        /// <summary>
+        /// 10-digit Mobile number of the customer
+        /// </summary>
+        public string Mobile
+        {
+            get => _mobile;
+            set
+            {
+                //mobile number should be 10 digit mobile number
+                if (value.Length == 10)
+                {
+                    _mobile = value;
+                }
+                else
+                {
+                    throw new CustomerException("Mobile number should be a 10-digit number");
+                }
+            }
+        }
+        #endregion
     }
 }
